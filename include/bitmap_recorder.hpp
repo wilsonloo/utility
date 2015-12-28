c75d6b9bd1575968d485b0c892f2758badc57767
wilsonloo <wilsonloo21@163.com>
2015-12-28 19:00:32 +0800

    1.添加 bitmap_recorder; 2.添加 offset 宏

---
+++ include/bitmap_recorder.hpp
@@ -0,0 +1,94 @@
+#ifndef EVL_UTILITY_BIT_MAP_RECORDER_HPP_
+#define EVL_UTILITY_BIT_MAP_RECORDER_HPP_
+
+////////////////////////////////////////////////////////////////////////////
+// USAGE:
+// subscript should > 0
+//////////////////////////////////////////////////////////////////////////
+
+#include <cassert>
+#include <boost/cstdint.hpp>
+
+namespace evl
+{
+	namespace utility
+	{
+		class BitMapRecorder
+		{
+			enum
+			{
+				MIN_ALIGN_SIZE = 8, // expecting 8 bit of char
+			};
+
+		public:
+			explicit BitMapRecorder(boost::uint32_t capacity) 
+				: m_capacity(capacity)
+				, m_data(NULL)
+			{
+				unsigned long charsNeeded = m_capacity / MIN_ALIGN_SIZE + 1;
+
+				m_data = new char[charsNeeded];
+				memset(m_data, 0, charsNeeded);
+			}
+
+			virtual ~BitMapRecorder( void)
+			{
+				if (m_data != NULL)
+				{
+					delete m_data;
+					m_data = NULL;
+				}
+			}
+
+			bool IsSet(boost::uint32_t what)
+			{
+				assert(what > 0 && what <= m_capacity && "out of bitmap range.");
+
+				char* whichByte = &(m_data[what / MIN_ALIGN_SIZE]);
+				return (whichByte[0] & (char)(1 << (what % MIN_ALIGN_SIZE))) != 0;
+			}
+
+			void Set(boost::uint32_t what)
+			{
+				assert(what > 0 && what <= m_capacity && "out of bitmap range.");
+
+				char* whichByte = &(m_data[what / MIN_ALIGN_SIZE]);
+				whichByte[0] = whichByte[0] | (char)(1 << (what % MIN_ALIGN_SIZE));
+			}
+
+			void UnSet(boost::uint32_t what)
+			{
+				assert(what > 0 && what <= m_capacity && "out of bitmap range.");
+
+				char* whichByte = &(m_data[what / MIN_ALIGN_SIZE]);
+				whichByte[0] = whichByte[0] & (char)(~(1 << (what % MIN_ALIGN_SIZE)));
+			}
+
+			bool GetNextDirtyIdx(boost::uint32_t& cur_idx)
+			{
+				if (cur_idx + 1 > get_capacity())
+					return false;
+
+				if (IsSet(cur_idx + 1))
+				{
+					++cur_idx;
+					return true;
+				}
+				
+				return false;
+			}
+    
+			inline boost::uint32_t get_capacity()const { return m_capacity; }
+
+		private:
+			BitMapRecorder( void);
+			BitMapRecorder( const BitMapRecorder& other);
+			BitMapRecorder& operator = ( const BitMapRecorder& other);
+
+			char* m_data;
+			boost::uint32_t m_capacity;
+		};
+	}
+}
+
+#endif // EVL_UTILITY_BIT_MAP_RECORDER_HPP_
---
+++ include/offset.hpp
@@ -0,0 +1,8 @@
+#ifndef EVL_UTILITY_OFFSET_HPP_
+#define EVL_UTILITY_OFFSET_HPP_
+
+#ifndef EVL_OFFSET_OF
+#define EVL_OFFSET_OF(TYPE, field_name) ((std::size_t)(&(((const TYPE*)0)->field_name)))
+#endif EVL_OFFSET_OF
+
+#endif // EVL_UTILITY_OFFSET_HPP_
