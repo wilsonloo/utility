74fbefcd17e6a2def385605ed267700c0920d2e4
wilson <wilsonloo21@163.com>
2017-10-30 19:15:54 +0800

    将观察者模式 去除 boost的使用

--- observer.hpp
+++ observer.hpp
@@ -1,84 +1,280 @@
 #ifndef EVL_UTILITY_OBSERVER_HPP_
 #define EVL_UTILITY_OBSERVER_HPP_
 
+/*
+	观察者模式
+
+	该模式的实现主要有两个类：Observer（观察者） 和 Observable（被观察者, 由ObjservableSubject 封装）
+	Observer 与 Observable 之间通过 Connection 进行连接，具体的操作已由 ObservableSubject 进行了封装 
+	Observer 通过虚函数 on_event() 提供了被观察者事件的回调封装，需要由子类进行覆盖实现。
+	以下是例子：
+
+	// 观察者主体
+	class Master : public evl::utility::Observer
+	{
+		// 重写观察者事件处理
+		virtual int on_event(const void* data, int param) override {
+		std::cout << "received : " << param << std::endl;
+
+		return 0;
+		}
+	};
+
+	具体使用：
+	{
+		// 被观察者对象
+		auto subject = new evl::utility::ObservableSubject();
+
+		// 观察者
+		auto master = new Master();
+
+		// master 向 subject 进行观察
+		subject->add_observer(master);
+
+		// subject 触发事件
+		subject->notify(nullptr, 22);
+		
+		// 测试运行结果
+		subject->dump();
+		master->dump();
+	}
+
+*/
+
 #include <set>
-#include <boost/signals2/signal.hpp>
-#include <boost/signals2/trackable.hpp>
-#include <boost/signals2/connection.hpp>
-#include <boost/function.hpp>
+#include <list>
+#include <map>
+#include <functional>
+#include <memory>
 
 namespace evl
 {
 	namespace utility
 	{
+		class Observable;
+		class Observer;
+
+		//
+		// 自连接器
+		// 作用：当被观察者、观察者断开时，自动释放连接；是全双工机制，不会出现单方向连接
+		//
+		class Connection
+		{
+			friend class Observable;
+			friend class Observer;
+
+		private:
+			// 断开连接后的销毁器
+			typedef std::function<void(Connection*)> ConnectionDisposerType;
+			typedef std::list<ConnectionDisposerType> DisposerCollectionType;
+			DisposerCollectionType disposers_;
+
+			// 断开连接
+			void disconnect()
+			{
+				for (auto dispose : disposers_) {
+					dispose(this);
+				}
+
+				disposers_.clear();
+			}
+
+			// 添加断开连接的销毁器
+			void add_dispose_handler(ConnectionDisposerType disposer) {
+				disposers_.push_back(disposer);
+			}
+		};
+		typedef std::shared_ptr<Connection> ConnectionPtr;
+
+		//
+		// 被观察者
+		//
+		class Observable
+		{
+		public:
+			//
+			// 事件处理器
+			//
+			typedef std::function<int(const void*, int)> EventHandlerType;
+
+			//
+			// 每个连接对应一个事件处理器
+			//
+			typedef std::map<ConnectionPtr, EventHandlerType> ConnectionCollectionType;
+
+		private:
+			// 连接列表
+			ConnectionCollectionType conns_;
+
+		private:
+			//////////////////////////////////////////////////////////////////////////
+			// 禁止所有拷贝构造函数、赋值函数，以免造成connection 的引用计数错误
+			explicit Observable(const Observable& other);
+			explicit Observable(const Observable* other);
+			Observable operator=(const Observable& other);
+			Observable operator=(const Observable* other);
+
+		public :
+			Observable() {
+
+			}
+
+			virtual ~Observable() {
+				disconnect_all();
+			}
+
+			// 全双工断开了全部链接
+			void disconnect_all() {
+				if (!conns_.empty()) {
+					for (ConnectionCollectionType::iterator iter = conns_.begin(); iter != conns_.end(); ) {
+						ConnectionPtr conn = iter->first;
+						conns_.erase(iter++);
+
+						conn->disconnect();
+					}
+				}
+			}
+
+			// 向全部观察者发送事件
+			void notify_all(const void* data, int param) {
+				if (conns_.empty())
+					return;
+				
+				for (ConnectionCollectionType::iterator iter = conns_.begin(); iter != conns_.end(); ++iter) {
+					EventHandlerType handler = iter->second;
+					handler(data, param);
+				}
+			}
+
+			ConnectionPtr create_connection(EventHandlerType observer) {
+				auto conn_ptr = std::shared_ptr<Connection>(new Connection());
+
+				// 绑定连接销毁器
+				conn_ptr->add_dispose_handler(std::bind(&Observable::connection_disposer, this, std::placeholders::_1));
+
+				// conn 绑定处理器
+				conns_.insert(std::make_pair(conn_ptr, observer));
+
+				return conn_ptr;
+			}
+
+			void connection_disposer(Connection* disposing_conn) {
+				if (disposing_conn == nullptr)
+					return;
+
+				// 从列表里移除 key 为 disposing_conn 的元素
+				for (ConnectionCollectionType::iterator iter = conns_.begin(); iter != conns_.end(); ) {
+					ConnectionPtr conn = iter->first;
+					if (conn.get() == disposing_conn) {
+						conns_.erase(iter++);
+					}
+					else {
+						++iter;
+					}
+				}
+			}
+
+			void dump()
+			{
+				std::cout << "connection count : " << conns_.size() << std::endl;
+			}
+		};
+
 		//
 		// 可观察者（可以观察其他对象）
 		// 
-		class TrackableObservable : public boost::signals2::trackable
+		class Observer
 		{
+		private:
+			// 此列表为了处理主动断开
+			typedef std::list<ConnectionPtr> ObservingTargetsCollectionType;
+			ObservingTargetsCollectionType observing_targets_;
+
+		private:
+			//////////////////////////////////////////////////////////////////////////
+			// 禁止所有拷贝构造函数、赋值函数，以免造成connection 的引用计数错误
+			explicit Observer(const Observer& other);
+			explicit Observer(const Observer* other);
+			Observer operator=(const Observer& other);
+			Observer operator=(const Observer* other);
+
 		public:
-			typedef boost::function<boost::int32_t(const void*, boost::uint32_t)> ObserveredEventHandlerType;
-			typedef std::set<boost::signals2::connection> ObservableListType;
 
-			TrackableObservable(ObserveredEventHandlerType event_handler)
-				:event_handler_(event_handler)
-			{}
-
-			~TrackableObservable()
+			Observer()
 			{
-				observable_conn_.clear();
+
 			}
 
-			inline bool insert_connection(const boost::signals2::connection& conn)
+			virtual ~Observer()
 			{
-				return observable_conn_.insert(conn).second;
-			}
-
-			// 返回连接数
-			inline boost::uint32_t num_conns()
-			{
-				// 清理废弃连接的操作
-				// 由于observable_conn_被动被boost::signal断开时没法获得通知
-				// 此列表>=boost::signals::trackable中的连接列表
-				ObservableListType::iterator it = observable_conn_.begin();
-				for (; it!=observable_conn_.end();)
-				{
-					if (!(*it).connected())
-						observable_conn_.erase(it++);
-					else
-						++it;
-				}
-
-				return observable_conn_.size();
-			}
-
-			inline boost::int32_t notify_event(const void* data_ptr, boost::uint32_t data_len)
-			{
-				return event_handler_(data_ptr, data_len);
+				disconnect_all();
 			}
 
 			// 断开所有(主动断开)
 			inline void disconnect_all()
 			{
-				ObservableListType::iterator it = observable_conn_.begin();
-				for (; it!=observable_conn_.end(); ++it)
+				for(ObservingTargetsCollectionType::iterator it = observing_targets_.begin(); it != observing_targets_.end(); )
 				{
-					if ((*it).connected())
-						(*it).disconnect();
+					ConnectionPtr conn_ptr = *it;
+					observing_targets_.erase(it++);
+
+					conn_ptr->disconnect();
 				}
-				observable_conn_.clear();
+
+				observing_targets_.clear();
 			}
 
-		private:
-			ObserveredEventHandlerType event_handler_;
-			// 此列表为了处理主动断开
-			ObservableListType observable_conn_;
+			// 事件处理
+			virtual int on_event(const void*, int) {
+				// todo 
+				return 0;
+			}
+
+			inline void insert_connection(ConnectionPtr conn_ptr)
+			{
+				// 绑定连接销毁器
+				conn_ptr->add_dispose_handler(std::bind(&Observer::connection_disposer, this, std::placeholders::_1));
+
+				observing_targets_.push_back(conn_ptr);
+
+			}
+
+			void connection_disposer(Connection* disposing_conn) {
+				if (disposing_conn == nullptr)
+					return;
+
+				// 从列表里移除 key 为 disposing_conn 的元素
+				for (ObservingTargetsCollectionType::iterator iter = observing_targets_.begin(); iter != observing_targets_.end(); ) {
+					ConnectionPtr conn = *iter;
+					if (conn.get() == disposing_conn) {
+						observing_targets_.erase(iter++);
+					}
+					else {
+						++iter;
+					}
+				}
+			}
+			
+			void dump()
+			{
+				std::cout << "observing target count : " << observing_targets_.size() << std::endl;
+			}
 		};
 
-		//
-		// 观察者列表
-		//
-		typedef boost::signals2::signal<boost::int32_t(const void*, boost::uint32_t)> ObserverListType;
+		class ObservableSubject : public Observable
+		{
+		public:
+			void add_observer(evl::utility::Observer* observer)
+			{
+				auto conn = create_connection(std::bind(&evl::utility::Observer::on_event, observer, std::placeholders::_1, std::placeholders::_2));
+				observer->insert_connection(conn);
+			}
+
+			void notify(const void* data, int param) {
+				notify_all(data, param);
+			}
+
+		};
 	}
 }
 
