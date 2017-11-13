a58c266f5b8bcce7a6eb451a4585733ed61bd343
wilson <wilsonloo21@163.com>
2017-11-13 19:36:19 +0800

    完善字符串辅助类：添加format函数

--- string_utils.hpp
+++ string_utils.hpp
@@ -23,24 +23,28 @@ namespace evl
             //////////////////////////////////////////////////////////////////////////
             // #pragma mark - Position
             /** Returns index of first occurrence of substring, otherwise returns -1. */
-            inline int indexOf(const std::string s, const std::string sub) {
+            inline int indexOf(const std::string& s, const std::string& sub) {
                 if (s == "" || sub == "" || sub.length() > s.length()) return -1;
                 auto pos = s.find(sub);
                 return (pos != std::string::npos) ? static_cast<int>(pos) : -1;
             }
+
             /** Returns index of last occurrence of substring, otherwise returns -1. */
             inline int lastIndexOf(const std::string &s, const std::string &sub) {
                 if (s == "" || sub == "" || sub.length() > s.length()) return -1;
                 auto it = std::find_end(std::begin(s), std::end(s), std::begin(sub), std::end(sub));
                 return (it != s.end()) ? static_cast<int>(it - std::begin(s)) : -1;
             }
+
             /** Returns whether a string begins with a substring. */
             inline bool startsWith(const std::string &s, const std::string &pre) { return s.find(pre) == 0; }
+            
             /** Returns whether a string ends with a substring. */
             inline bool endsWith(const std::string &s, const std::string &suf) {
                 if (s.length() < suf.length()) return false;
                 return s.compare(s.length() - suf.length(), suf.length(), suf) == 0;
             }
+
             /** Returns whether a string contains a certain substring. */
             inline bool includes(const std::string &s, const std::string &sub) {
                 return indexOf(s, sub) != -1;
@@ -48,41 +52,48 @@ namespace evl
 
             //////////////////////////////////////////////////////////////////////////
             // #pragma mark - Casing
-                            /** Converts a string to lowercase. */
-            inline std::string toLowerCase(std::string s) {
+            /** Converts a string to lowercase. */
+            inline std::string toLowerCase(const std::string& s) {
                 std::transform(std::begin(s), std::end(s), std::begin(s), ::tolower);
                 return s;
             }
             /** Convers a string to uppercase. */
-            inline std::string toUpperCase(std::string s) {
+            inline std::string toUpperCase(const std::string& s) {
                 std::transform(std::begin(s), std::end(s), std::begin(s), ::toupper);
                 return s;
             }
 
             //////////////////////////////////////////////////////////////////////////
             // #pragma mark - Trimming
-                            /** Trims whitespace at the beginning of a string. */
-            inline std::string trimLeft(std::string s) {
+            /** Trims whitespace at the beginning of a string. */
+            inline std::string trimLeft(const std::string& str) {
+                std::string s(str);
+
                 s.erase(std::begin(s),
                     std::find_if(std::begin(s),
                         std::end(s),
                         std::not1(std::ptr_fun<int, int>(std::isspace))));
                 return s;
             }
+
             /** Trims whitespace at the end of a string. */
-            inline std::string trimRight(std::string s) {
+            inline std::string trimRight(const std::string& str) {
+                std::string s(str);
+
                 s.erase(std::find_if(s.rbegin(),
                     s.rend(),
                     std::not1(std::ptr_fun<int, int>(std::isspace))).base(),
                     std::end(s));
                 return s;
             }
+
             /** Trims whitespace at the end of a string, starting from a delimiter. */
-            inline std::string trimRight(const std::string s, const std::string &delim) {
+            inline std::string trimRight(const std::string& s, const std::string &delim) {
                 return s.substr(0, s.find(delim));
             }
+
             /** Trims whitespace to the beginning and end of a string. */
-            inline std::string trim(const std::string s) { return trimRight(trimLeft(s)); }
+            inline std::string trim(const std::string& s) { return trimRight(trimLeft(s)); }
 
             
             /** Removes whitespace from a string. */
@@ -97,7 +108,7 @@ namespace evl
 
             //////////////////////////////////////////////////////////////////////////
             // #pragma mark - Splitting
-                            /** Splits strings based on max line length. */
+            /** Splits strings based on max line length. */
             inline std::vector<std::string> split(const std::string &s, const size_t maxLen) {
                 std::vector<std::string> strs;
                 for (size_t i = 0; i < s.length(); i += maxLen) {
@@ -105,6 +116,7 @@ namespace evl
                 }
                 return strs;
             }
+
             /** Splits strings based on a delimiter. */
             inline std::vector<std::string> split(const std::string &s, const std::string &delim = "\n") {
                 std::vector<std::string> strs;
@@ -121,8 +133,10 @@ namespace evl
 
             //////////////////////////////////////////////////////////////////////////
             // #pragma mark - Replacement
-                            /** Replaces all occurrences of a search with a replacement substring. */
-            inline std::string replace(std::string s, const std::string &search, const std::string &r) {
+            /** Replaces all occurrences of a search with a replacement substring. */
+            inline std::string replace(const std::string& str, const std::string &search, const std::string &r) {
+                std::string s(str);
+
                 std::size_t pos = 0;
                 while ((pos = s.find(search, pos)) != std::string::npos) {
                     s.replace(pos, search.length(), r);
@@ -130,15 +144,43 @@ namespace evl
                 }
                 return s;
             }
+
             /** Replaces all occurrences of multiple searches with a replacement substring. */
-            inline std::string replace(std::string s, std::initializer_list<std::string> searches, const std::string &r) {
+            inline std::string replace(const std::string& str, std::initializer_list<std::string> searches, const std::string &r) {
+                std::string s(str);
+
                 for (auto &i : searches) { s = replace(s, i, r); }
                 return s;
             }
+
             /** Replaces everything after a maxLen in a string with a replacement substring (default: ""). */
-            inline std::string trunc(std::string s, const size_t maxLen, const std::string &r = "") {
+            inline std::string trunc(const std::string& str, const size_t maxLen, const std::string &r = "") {
+                std::string s(str);
+
                 if (s.length() > maxLen && maxLen > 0) s = s.substr(0, maxLen) + r;
                 return s;
+            }
+
+            //////////////////////////////////////////////////////////////////////////
+            // #param mark - Formating
+            inline std::string format(const char* format, ...)
+            {
+                // get the data-length after formated
+                va_list args;
+                va_start(args, format);
+                int count = _vsnprintf(nullptr, 0, format, args);
+                va_end(args);
+
+                va_start(args, format);
+                char* buf = (char*)malloc(count * sizeof(char));
+                _vsnprintf(buf, count, format, args);
+                va_end(args);
+
+                std::string str(buf);
+                free(buf);
+                buf = nullptr;
+
+                return str;
             }
         }// namespace string_utils
       } // namespace utility
