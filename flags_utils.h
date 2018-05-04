364914d33bc68839837213980186647e41119a31
wilsonloo <wilsonloo21@163.com>
2018-05-04 10:31:35 +0800

    添加 重复声明的 宏控制

--- flags_utils.h
+++ flags_utils.h
@@ -1,6 +1,7 @@
 #ifndef EVL_UTILITY_FLAGS_UTIL
 #define EVL_UTILITY_FLAGS_UTIL
 
+#ifndef INSERT_FLAG
 #define INSERT_FLAG(source, flag) \
 	((source) = (unsigned int)(source) | (unsigned int)(flag))
 
@@ -9,5 +10,7 @@
 
 #define HAS_FLAG(source, flag) \
 	(((source) & (unsigned int)(flag)) != 0)
+
+#endif // 
 
 #endif	//EVL_UTILITY_FLAGS_UTIL 
